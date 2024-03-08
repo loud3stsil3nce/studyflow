890698528e0583a670e2721df40f24cd624c4766
Loud3stsil3nce <rafiurrahman325@gmail.com>
2024-03-08 07:49:30 -0500

    created a login box. Will create authentication for the site.

--- studyflow/src/components/Navbar.jsx
+++ studyflow/src/components/Navbar.jsx
@@ -3,11 +3,10 @@ export default function Navbar() {
     return <nav className="nav">
         <a href="/" className="site-title">STUDYFLOW</a>
         <ul>
-            <li>
-                <a href="/Home">Home</a>
-                <a href="/About">About</a>
-                <a href="/Login">Login</a>
-            </li>
+            <li className="active"><a href="/Home">Home</a></li>
+            <li><a href="/About">About</a></li>
+            <li><a href="/Login">Login</a></li>
+            
         </ul>
 
     </nav>
--- studyflow/src/pages/LoginPage.jsx
+++ studyflow/src/pages/LoginPage.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react"
 import Navbar from "../components/Navbar";
-
+import LoginBox from "../components/LoginBox";
 
 export default function LoginPage() {
     return (
         <>
         <Navbar />
-        <h2>Login!</h2>
+        <div className="loginheader">Login!</div>
+        <LoginBox />
         </>
     )
 }
