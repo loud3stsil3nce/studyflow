5106737d45db13d4ac991ac0669917d98271af31
Loud3stsil3nce <rafiurrahman325@gmail.com>
2024-03-13 00:40:42 -0400

    Added basic html and css for canvas.

--- studyflow/src/components/Sketch/Sketchpad.jsx
+++ studyflow/src/components/Sketch/Sketchpad.jsx
@@ -1,9 +1,16 @@
 import { Grid, GridItem, Button, Link} from '@chakra-ui/react'
 
 const Canvas = () => {
-<>
-<p>Hey</p>
-</>
-}
+    const gridContainer = document.querySelector('#divContainer');
 
+
+return (
+
+<Grid>
+    <p>CANVAS HERE</p>
+</Grid>
+
+
+)
+}
 export default Canvas
